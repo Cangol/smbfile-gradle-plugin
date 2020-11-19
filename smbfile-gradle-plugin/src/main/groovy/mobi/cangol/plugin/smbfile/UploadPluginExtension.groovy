4ce5e6f95f9fbb285ee2e5ac07c7364dab3ab280
Cangol <wxw404@gmail.com>
2020-11-19 18:17:32 +0800

    update

--- smbfile-gradle-plugin/src/main/groovy/mobi/cangol/plugin/smbfile/UploadPluginExtension.groovy
+++ smbfile-gradle-plugin/src/main/groovy/mobi/cangol/plugin/smbfile/UploadPluginExtension.groovy
@@ -12,4 +12,5 @@ class UploadPluginExtension {
     String pushDir
     String trialDir
     String testDir
+    String flag
 }
--- smbfile-gradle-plugin/src/main/groovy/mobi/cangol/plugin/smbfile/UploadTask.groovy
+++ smbfile-gradle-plugin/src/main/groovy/mobi/cangol/plugin/smbfile/UploadTask.groovy
@@ -20,17 +20,22 @@ class UploadTask extends DefaultTask {
         def apkOutput = variant.outputs.find { variantOutput -> variantOutput instanceof ApkVariantOutput }
 
         String apkPath = apkOutput.outputFile.getAbsolutePath()
-        log.info("apkPath ===> " + apkPath)
+        log.error("apkPath ===> " + apkPath)
 
         def destDirPath = extension.getProperty(variant.buildType.name + "Dir");
         if (destDirPath == null) {
             destDirPath = ""
         }
-        log.info("destDirPath ===> " + destDirPath)
 
-        def fileDir = "V" + variant.versionName + "." + variant.versionCode + "_" + new Date().format("yyyy-MM-dd_HH-mm-ss", TimeZone.getTimeZone("GMT+8"));
+        def flag=""
+        if (!extension.flag.equalsIgnoreCase("")) {
+            flag="_"+extension.flag
+        }
+        log.error("flag ===> " + flag)
+
+        def fileDir = "V" + variant.versionName+ "." + variant.versionCode+ "_" + new Date().format("yyyy-MM-dd_HH-mm-ss", TimeZone.getTimeZone("GMT+8"))+flag
         def destDir = destDirPath + "/" + fileDir
-        log.info("destDir ===> " + destDir)
+        log.error("destDir ===> " + destDir)
 
         def path = client.upload(destDir, apkOutput.outputFile.name, apkPath)
         log.info("upload ===> " + path)
